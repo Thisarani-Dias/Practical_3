31b22854b4213301632c04aa8436db3a8385b540
Thisarani Dias <140165499+Thisarani-Dias@users.noreply.github.com>
2024-07-01 12:53:30 +0530

    Add files via upload

---
+++ mean.scala
@@ -0,0 +1,16 @@
+package q3
+
+object mean {
+  def mean(n1:Double, n2:Double):Double = {
+    val tot = n1+n2
+    val mean = tot/2
+    val rounded = f"$mean%.2f".toDouble
+    rounded
+  }
+  def main(args:Array[String]): Unit = {
+    var n1 = 5.34
+    var n2 = 2.33
+    var res = mean(n1, n2)
+    println(res)
+  }
+}
